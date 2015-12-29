e66b4afc619f282d6ed67a513183546283ec2b4e
sinharahul <sinharahul@gmail.com>
2015-12-28 20:21:04 -0500

    elequent javascript

---
+++ prompt.js
@@ -0,0 +1,21 @@
+/**
+ * http://usejsdoc.org/
+ */
+ var prompt = require('prompt');
+var theNumber;
+  prompt.start();
+
+  prompt.get(['number'], function (err, result) {
+    if (err) { return onErr(err); }
+    console.log('Command-line input received:');
+    console.log('number ' + result.number);
+    theNumber=result.number
+  });
+
+  function onErr(err) {
+    console.log(err);
+    return 1;
+  }
+var no = Number(theNumber);
+console.log("The number received is ",no)
+
