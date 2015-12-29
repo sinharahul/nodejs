abd5fadda0a992c359a3d3cf84ab146a40b0894f
sinharahul <sinharahul@gmail.com>
2015-12-29 17:14:45 -0500

    range done!!

--- range.js
+++ range.js
@@ -3,5 +3,11 @@
  * that produces the range
  */
 function range(start,end){
-	
+	if(start > end)return
+	var arr=[]	
+	for(var i=start;i<=end;i++ ){
+		arr.push(i)
+	}	
+	return arr
 }
+console.log(range(1,100))
